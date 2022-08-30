342089247201c56da54e798ff15259067e147bdd
Wind0317 <52394193+Wind0317@users.noreply.github.com>
2022-08-30 15:18:57 +0800

    0830-1

---
+++ 0826/cinema.js
@@ -0,0 +1,445 @@
+var cinema = [
+  {
+    id: 6092,
+    mark: 0,
+    nm: "汉南影城",
+    sellPrice: "23",
+    addr: "汉南区汉南大道315号武商量贩五楼",
+    distance: "887.5km",
+    tag: {
+      allowRefund: 1, // 退
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1, // 改
+      hallTypeVOList: [],
+      sell: 1, // 有票
+      snack: 1, // 小吃
+      vipTag: "折扣卡" // 折扣卡
+    },
+    promotion: {}
+  },
+  {
+    id: 24570,
+    mark: 0,
+    nm: "天河影城汉南店",
+    sellPrice: "23",
+    addr: "汉南区纱帽正街“大学城金街”商业体3楼",
+    distance: "888.9km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promoion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减4元"
+    }
+  }, {
+    id: 6247,
+    mark: 0,
+    nm: "大地影院（新宜佳店）",
+    sellPrice: "28.9",
+    addr: "江夏区兴新街136号宜佳广场四楼东侧（近中心百货）",
+    distance: "891.9km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 0,
+      hallType: ["4K厅"],
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单1张最高立减10元"
+    }
+  }, {
+    id: 2069,
+    mark: 0,
+    nm: "银兴国际影城（江夏大道店）",
+    sellPrice: "8",
+    addr: "江夏区江夏大道128号附1号中央大街2号楼4层",
+    distance: "892.5km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 0,
+      hallType: ["杜比全景声厅"],
+      hallTypeVOList: [{
+        name: "杜比全景声厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减6元"
+    }
+  }, {
+    id: 16292,
+    mark: 0,
+    nm: "金逸影城（江夏店）",
+    sellPrice: "31.9",
+    addr: "江夏区江夏大道与北华街交汇处九全嘉购物广场4楼",
+    distance: "893.1km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 0,
+      hallType: ["杜比全景声厅", "DTS:X 临境音厅"],
+      hallTypeVOList: [{
+        name: "杜比全景声厅",
+        url: ""
+      }, {
+        name: "DTS:X 临境音厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减4元"
+    }
+  }, {
+    id: 10880,
+    mark: 0,
+    nm: "高德国际影城（江夏中百广场店）",
+    sellPrice: "15",
+    addr: "江夏区文华路37号中百广场6楼",
+    distance: "893.8km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 25395,
+    mark: 0,
+    nm: "华夏影城江夏店",
+    sellPrice: "26.9",
+    addr: "江夏区纸坊街道谭鑫路17号万豪世纪天街商业广场3楼1号",
+    distance: "894.1km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallType: ["杜比全景声厅", "4K厅", "巨幕厅"],
+      hallTypeVOList: [{
+        name: "杜比全景声厅",
+        url: ""
+      }, {
+        name: "4K厅",
+        url: ""
+      }, {
+        name: "巨幕厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 13066,
+    mark: 0,
+    nm: "万达影城（江夏MALL店）",
+    sellPrice: "16",
+    addr: "江夏区文化大道联投广场三层",
+    distance: "894.8km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallType: ["4DX厅", "儿童厅"],
+      hallTypeVOList: [{
+        name: "4DX厅",
+        url: ""
+      }, {
+        name: "儿童厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 0,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单1张最高立减5元"
+    }
+  }, {
+    id: 24877,
+    mark: 0,
+    nm: "中影烽禾影城（罗马春天奥莱店）",
+    sellPrice: "28",
+    addr: "江夏区江夏大道罗马春天奥特莱斯商业城STU区四楼",
+    distance: "896.3km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 0,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 0
+    },
+    promotion: {}
+  }, {
+    id: 9326,
+    mark: 0,
+    nm: "东汉影城",
+    sellPrice: "33",
+    addr: "江夏区金樱街鸿发大厦A座",
+    distance: "899.8km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 0,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减4元"
+    }
+  }, {
+    id: 14522,
+    mark: 0,
+    nm: "高德国际影城（藏龙岛店）",
+    sellPrice: "37",
+    addr: "江夏区杨桥湖大道13号阳光时尚金泰商务酒店后面三楼（想唱就唱楼上）",
+    distance: "899.8km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallType: ["杜比全景声厅"],
+      hallTypeVOList: [{
+        name: "杜比全景声厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 9753,
+    mark: 0,
+    nm: "银兴影城（湖经店）",
+    sellPrice: "28",
+    addr: "江夏区藏龙岛开发区拓创产业园",
+    distance: "900.3km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 0,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 13673,
+    mark: 0,
+    nm: "东澜岸天河国际影城",
+    sellPrice: "33",
+    addr: "洪山区青菱乡黄家湖西路福星惠誉东澜岸广场1号楼2楼（武汉科技大学黄家湖校区）",
+    distance: "901.3km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 25947,
+    mark: 0,
+    nm: "银兴国际影城同兴店",
+    sellPrice: "31",
+    addr: "江夏区同兴生活广场",
+    distance: "901.6km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 16227,
+    mark: 0,
+    nm: "风云影城（江夏店）",
+    sellPrice: "31",
+    addr: "江夏区藏龙岛光谷大道百度百捷大楼四楼",
+    distance: "901.8km",
+    tag: {
+      allowRefund: 0,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 16864,
+    mark: 0,
+    nm: "中影星美浩瀚国际影城（常福店）",
+    sellPrice: "33",
+    addr: "蔡甸区常福商务广场1S三楼中影星美浩瀚国际影城",
+    distance: "903.2km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减6元"
+    }
+  }, {
+    id: 15242,
+    mark: 0,
+    nm: "幻幕影城",
+    sellPrice: "33",
+    addr: "江夏区流芳街武汉工程大学泰塑公寓",
+    distance: "904.3km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 13813,
+    mark: 0,
+    nm: "锦绣龙城华幕影城",
+    sellPrice: "31",
+    addr: "洪山区民族大道龙城路龙城商业街（北区物业旁）",
+    distance: "905.2km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallTypeVOList: [],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减6元"
+    }
+  }, {
+    id: 15780,
+    mark: 0,
+    nm: "中影星美国际影城（创意天地店）",
+    sellPrice: "28",
+    addr: "洪山区野芷湖西路16号创意剧场（保利心语）",
+    distance: "905.6km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallType: ["杜比全景声厅", "4D厅"],
+      hallTypeVOList: [{
+        name: "杜比全景声厅",
+        url: ""
+      }, {
+        name: "4D厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {}
+  }, {
+    id: 1634,
+    mark: 0,
+    nm: "华谊兄弟影院（光谷IMAX店）",
+    sellPrice: "31",
+    addr: "洪山区关山大道光谷天地F1区3楼（南湖大道口）",
+    distance: "906.3km",
+    tag: {
+      allowRefund: 1,
+      buyout: 0,
+      cityCardTag: 0,
+      cityCardTag: 0,
+      endorse: 1,
+      hallType: ["IMAX厅"],
+      hallTypeVOList: [{
+        name: "IMAX厅",
+        url: ""
+      }],
+      sell: 1,
+      snack: 1,
+      vipTag: "折扣卡"
+    },
+    promotion: {
+      "cardPromotionTag": "开卡特惠，首单2张最高立减12元"
+    }
+  }
+]
---
+++ 0826/stus.js
@@ -0,0 +1,38 @@
+var stus = [
+    {
+        stuName: "张三",
+        sex: "女",
+        age: 18,
+        hobby: ["看书", "睡觉"]
+    },
+    {
+        stuName: "李四",
+        sex: "女",
+        age: 22,
+        hobby: ["看书", "睡觉"]
+    },
+    {
+        stuName: "王五",
+        sex: "男",
+        age: 18,
+        hobby: ["看书", "睡觉"]
+    },
+    {
+        stuName: "赵六",
+        sex: "男",
+        age: 30,
+        hobby: ["看书", "睡觉"]
+    },
+    {
+        stuName: "田七",
+        sex: "女",
+        age: 19,
+        hobby: ["看书", "睡觉"]
+    },
+    {
+        stuName: "周八",
+        sex: "女",
+        age: 22,
+        hobby: ["看书", "睡觉"]
+    }
+]
